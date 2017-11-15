19156f7bd75a7338005f036cd219d1a2534a4525
Caio <caio.dscamara@gmail.com>
2017-11-15 03:33:37 -0200

    Adicionando o helper_service para ajudar o usuário.

---
+++ services/help_service.rb
@@ -0,0 +1,9 @@
+class HelpService
+  def self.call
+    response  = "O que eu sei fazer : \n"
+    response += "Help\n\n"
+    response += "> Lista de comandos que eu conheço\n"
+    #...
+    response
+  end
+end
